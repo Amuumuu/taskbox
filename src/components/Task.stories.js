bd4df2fa069610984769f6499da42aa1bb38c92d
wangjiabiao <wangjiabiao@ykccn.com>
2022-02-18 21:00:42 +0800

    chore: simple-component

---
+++ src/components/Task.stories.js
@@ -0,0 +1,53 @@
+import Task from './Task.vue';
+
+import { action } from '@storybook/addon-actions';
+
+export default {
+  component: Task,
+//   以Data结尾的导出不作为story
+  excludeStories: /.*Data$/,
+  title: 'Task',
+  // 事件将会被映射到UI上
+  argTypes: {
+    onPinTask: {},
+    onArchiveTask: {},
+  },
+};
+
+export const actionsData = {
+  onPinTask: action('pin-task'),
+  onArchiveTask: action('archive-task'),
+};
+
+const Template = args => ({
+  components: { Task },
+  setup() {
+    return { args, ...actionsData };
+  },
+  template: '<Task v-bind="args" />',
+});
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
